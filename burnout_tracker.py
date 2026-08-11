b74621e6d9d0452ffd345c9d3d0fde9b332f9094
Teodor Iordan <teodor.iordan2131@gmail.com>
2026-08-11 02:17:42 +0100

    Add Check_In class to represent a daily check-in (date, mood, energy, workload), with daily_check_in() to format it for saving to check_ins.txt

--- burnout_tracker.py
+++ burnout_tracker.py
@@ -1,3 +1,15 @@
 import tkinter as tk
 from tkinter import messagebox
 import datetime
+
+FILENAME = "check_ins.txt"
+
+class Check_In:
+    def __init__(self, date, mood, energy, workload):
+        self.date = date
+        self.mood = mood
+        self.energy = energy
+        self.workload = workload
+
+    def daily_check_in(self):
+        return f"{self.date},{self.workload},{self.energy},{self.mood}"
